b6f25a913de7f7df6f8068b180109eabcf201d55
Blessing <ayeniblessing32@gmail.com>
2017-06-08 04:59:41 +0100

    Add class method

--- oop/src/car.js
+++ oop/src/car.js
@@ -1,50 +1,49 @@
-function Car(name, wheels, make, model, year, status, sold_on)
+class Car()
 {
-	this.name = name;
-	this.wheels = 4;
-	this.make = make;
-	this.model = model;
-	this.year = year;
-	this.sold_on = sold_on;
-	this.status = 'packed';
+	constructor(name, wheels, make, model, year, status, sold_on){
+
+		this.name = name;
+		this.wheels = 4;
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		this.sold_on = sold_on;
+		this.status = 'packed';
+		
+		if(name === undefined) {
+			this.name = 'Mercedez Benz';
+		}
+		if(model === undefined) {
+			this.model = 'A-Class';
+		}
+
+	}
+	move()
+	{
+		this.isSpeeding = false;
+		//abstraction
+		this.move = function() {
+			var speed = 20;
+		}
+		//--end
+		this.isMoving = true;
+		this.status = 'moving';
+	}
+
 	
-	if(name === undefined) {
-		this.name = 'Mercedez Benz';
-	}
-	if(model === undefined) {
-		this.model = 'A-Class';
+	speed() {
+		this.isMoving = false;
+		//abstraction
+		this.move = function() {
+			var speed = 20;
+		}
+		//--end
+		this.isSpeeding = true;
+		this.status = 'speeding';
 	}
 }
 
-Car.prototype.name = function(name) {
-	this.name = name;
-}
 
-Car.prototype.model = function(model) {
-	this.model = model;
-}
-
-Car.prototype.move = function() {
-	this.isSpeeding = false;
-	//abstraction
-	this.move = function() {
-		var speed = 20;
-	}
-	//--end
-	this.isMoving = true;
-	this.status = 'moving';
-}
-
-Car.prototype.speed = function() {
-	this.isMoving = false;
-	//abstraction
-	this.move = function() {
-		var speed = 20;
-	}
-	//--end
-	this.isSpeeding = true;
-	this.status = 'speeding';
-}
 
 function Truck(name, wheels, make, model, year, status, sold_on)
 {
